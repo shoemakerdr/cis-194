00cc15f7a93a05dc9dc4e2455dbe6195fed0c84e
Derek Shoemaker <shoemakerdr@gmail.com>
2018-01-15 00:45:34 -0600

    Finished exercise 5 tower of hanoi problem

--- Homework1.hs
+++ Homework1.hs
@@ -61,3 +61,23 @@ reduceDoubleDigits (x : xs) =
 validate :: Integer -> Bool
 validate card =
   mod (sumDigits $ doubleEveryOther $ toDigits card) 10 == 0
+
+
+
+-- EXERCISE 5 --
+
+
+type Peg = String
+
+type Move = (Peg, Peg)
+
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi n a b c =
+  let
+    step1 = hanoi (n - 1) a c b
+    step2 = [(a, b)]
+    step3 = hanoi (n - 1) c b a
+  in
+    step1 ++ step2 ++ step3
