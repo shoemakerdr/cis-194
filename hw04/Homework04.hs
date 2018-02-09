13bdbbf4848c66e1a85f0333fea73374a4fc027d
Derek Shoemaker <shoemakerdr@gmail.com>
2018-02-09 00:34:11 -0600

    Finished homework 4

--- hw04/Homework04.hs
+++ hw04/Homework04.hs
@@ -1,3 +1,5 @@
+module Homework04 where
+
 import Data.List
 
 
@@ -5,22 +7,24 @@ import Data.List
 -- EXERCISE 1
 
 
--- fun1 :: [Integer] -> Integer
--- fun1 [] = 1
--- fun1 (x:xs)
---   | even x = (x - 2) * fun1 xs
---   | otherwise = fun1 xs
+{-| Reimplement each of the following functions:
 
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+  | even x = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n
+  | even n = n + fun2 (n `div` 2)
+  | otherwise = fun2 (3 * n + 1)
+
+-}
 
 fun1 :: [Integer] -> Integer
 fun1 = foldl' (\x y -> x * (y - 2)) 1 . filter even
-
-
--- fun2 :: Integer -> Integer
--- fun2 1 = 0
--- fun2 n 
---   | even n = n + fun2 (n `div` 2)
---   | otherwise = fun2 (3 * n + 1)
 
 
 fun2 :: Integer -> Integer
@@ -76,4 +80,34 @@ treeHeight (Node height _ _ _) = height
 -- EXERCISE 3
 
 
+-- 1. Implement a function
+xor :: [Bool] -> Bool
+xor = odd . (foldl (\x y -> if y then x+1 else x) 0)
+
+
+-- 2. Implement map as a fold.
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x y -> ((f x) : y)) []
+
+
+
+-- EXERCISE 4
+
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n =
+  let
+    end =
+      2 * n + 2
+    sundaram =
+      map (\(x,y) -> x + y + (2 * x * y)) $
+        cartProd [1..end] [1..end]
+    sieve =
+      filter (\x -> not (x `elem` sundaram)) [1..]
+  in
+   takeWhile (\x -> x <= end) $ map (\x -> 2 * x + 1) sieve
+
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
 
